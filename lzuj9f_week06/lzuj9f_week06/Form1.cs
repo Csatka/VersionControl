1a463c294473852d28753549e90788ce6dffb2e7
Csatka <kcsatai@gmail.com>
2020-10-18 13:04:19 +0200

    6

--- lzuj9f_week06/lzuj9f_week06/Form1.cs
+++ lzuj9f_week06/lzuj9f_week06/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace lzuj9f_week06
@@ -20,10 +21,28 @@ namespace lzuj9f_week06
         {
             InitializeComponent();
 
-            
+
 
             dataGridView1.DataSource = Rates;
             ProcessXML();
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            
+            series.BorderWidth = 2;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void ProcessXML()
